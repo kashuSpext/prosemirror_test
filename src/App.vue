6f8d3e00ed7daeeeec96b3b177ed7b24559ea1c0
Ashutosh Kaushik <ashutosh@clearfeed.ai>
2022-01-17 05:03:21 +0530

    add: working example

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Editor />
+  </div>
+</template>
+
+<script>
+import Editor from "./components/Editor";
+
+export default {
+  name: "App",
+  components: {
+    Editor,
+  },
+};
+</script>
